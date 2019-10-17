19552313ac7f0d176e7474c3eaec1d201c176342
William Dutra <williamdutra21@gmail.com>
2019-10-16 23:45:23 -0300

    layout login finished

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,15 +1,40 @@
 import React, {Component} from 'react';
 
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
+
+import {
+  Container,
+  HeaderView,
+  TextHeader,
+  ImageView,
+  FormView,
+  Image,
+  TextTitleInput,
+  TextInput,
+  TouchableOpacity,
+  TextTouch,
+} from './styles';
 
 // import { Container } from './styles';
 
 export default class Login extends Component {
   render() {
     return (
-      <View>
-        <Text>LOGIN</Text>
-      </View>
+      <Container>
+        <HeaderView>
+          <TextHeader>TAREFA FÁCIL</TextHeader>
+        </HeaderView>
+        <ImageView>
+          <Image source={require('~/images/tarefa.png')} />
+        </ImageView>
+        <FormView>
+          <TextTitleInput>Nome</TextTitleInput>
+          <TextInput placeholder="Digite seu nome" />
+          <TouchableOpacity>
+            <TextTouch>ENTRAR</TextTouch>
+          </TouchableOpacity>
+        </FormView>
+      </Container>
     );
   }
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,3 +1,59 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: white;
+`;
+
+export const HeaderView = styled.View`
+  flex-direction: row;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TextHeader = styled.Text`
+  font-size: 25;
+  color: gray;
+  font-weight: bold;
+`;
+
+export const ImageView = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Image = styled.Image`
+  width: 300;
+  height: 300;
+`;
+
+export const FormView = styled.View`
+  flex: 2;
+  margin-left: 12;
+  margin-right: 12;
+  justify-content: center;
+`;
+
+export const TextTitleInput = styled.Text`
+  font-size: 16;
+  color: gray;
+`;
+
+export const TextInput = styled.TextInput`
+  font-size: 18;
+`;
+
+export const TouchableOpacity = styled.TouchableOpacity`
+  background-color: green;
+  height: 50;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20;
+`;
+
+export const TextTouch = styled.Text`
+  font-size: 16;
+  color: white;
+`;
